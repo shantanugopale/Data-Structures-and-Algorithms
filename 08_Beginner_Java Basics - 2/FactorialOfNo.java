d8530667d116e2b0d8ec24bf7346e6aef6562e30
shantanugopale <gopaleshantanu@gmail.com>
2025-01-07 14:46:25 +0530

    Java Basics-2 Section with Codebase and Notes Added

--- 08_Beginner_Java Basics - 2/FactorialOfNo.java
+++ 08_Beginner_Java Basics - 2/FactorialOfNo.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 public class FactorialOfNo {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the Number : ");
+            int num = sc.nextInt();
+            int fact = 1;
+            int i = 1;
+            while (i <= num) {
+                fact = fact * i;
+                i++;
+            }
 
-        System.out.println("Enter the Number : ");
-        int num = sc.nextInt();
-        int fact = 1;
-        int i = 1;
-        while (i <= num) {
-            fact = fact * i;
-            i++;
+            System.out.println("Factorial is : " + fact);
         }
-
-        System.out.println("Factorial is : " + fact);
     }
 }
