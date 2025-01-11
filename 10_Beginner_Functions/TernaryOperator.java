347d3a576f8515a7eadce515a5a87581fc0f5682
shantanugopale <gopaleshantanu@gmail.com>
2025-01-11 23:05:13 +0530

    Codebase added

---
+++ 10_Beginner_Functions/TernaryOperator.java
@@ -0,0 +1,18 @@
+
+public class TernaryOperator {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 5;
+
+        int res = (x > y) ? x : y;
+
+        System.out.println(res);
+
+        int a = 0;
+        int b = -1;
+
+        int result = (a > b) ? a : b;
+    }
+}
