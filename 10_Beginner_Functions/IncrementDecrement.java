08a39b5f6f37974a913e5ff0a5d2600bda0fbdba
shantanugopale <gopaleshantanu@gmail.com>
2025-01-11 23:04:55 +0530

    Codebase added

---
+++ 10_Beginner_Functions/IncrementDecrement.java
@@ -0,0 +1,14 @@
+
+public class IncrementDecrement {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = x++;
+
+        int z = ++y;
+
+        System.out.println("x = " + x + " y= " + y + "z = " + z
+        );
+    }
+}
