1db422b655a34424b2609f04ada21a61e188c33f
shantanugopale <gopaleshantanu@gmail.com>
2025-01-14 22:11:01 +0530

    Repeating Element index Efficient Solution Added

---
+++ 12_Beginner_Java-1D and 2D Arrays/EfficientWay_ReturnIndex_RepeatingElements_Array_4.java
@@ -0,0 +1,43 @@
+/*
+        Enter the Element to Check and Return Index : 
+        3
+        0 2 5 7
+ */
+
+import java.util.Scanner;
+
+class EfficientWay_ReturnIndex_RepeatingElements_Array_4 {
+
+    static int[] efficientWayReturnIndexOfRepeatingElement(int[] arr, int element) {
+
+        int n = arr.length;
+
+        int[] output = new int[n + 1];
+        int j = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == element) {
+                output[j] = i;
+                j++;
+            }
+        }
+        output[j] = -1;
+        return output;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[]{3, 2, 3, 4, 5, 3, 8, 3};
+
+        System.out.println("Enter the Element to Check and Return Index : ");
+        int element = sc.nextInt();
+
+        int[] output = efficientWayReturnIndexOfRepeatingElement(arr, element);
+
+        for (int i = 0; output[i] != -1; i++) {
+            System.out.print(output[i] + " ");
+        }
+    }
+}
