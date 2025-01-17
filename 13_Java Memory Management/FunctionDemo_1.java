e960a8be0e7fabc4e34ed538c778c57e50b7f081
shantanugopale <gopaleshantanu@gmail.com>
2025-01-17 23:21:27 +0530

    function Demo codebase Added

---
+++ 13_Java Memory Management/FunctionDemo_1.java
@@ -0,0 +1,15 @@
+
+class FunctionDemo_1 {
+
+    static void add() {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println("Sum is: " + c);
+    }
+
+    public static void main(String[] args) {
+
+        add();
+    }
+}
