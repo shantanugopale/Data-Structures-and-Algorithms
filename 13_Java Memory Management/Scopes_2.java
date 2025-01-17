0977ccb444f1af9671e7729f9799f4967c7c4f2d
shantanugopale <gopaleshantanu@gmail.com>
2025-01-17 23:26:55 +0530

    Scopes codebase Added

---
+++ 13_Java Memory Management/Scopes_2.java
@@ -0,0 +1,13 @@
+
+class Scopes_2 {
+
+    int a = 10; // instance variable
+
+    public static void main(String[] args) {
+
+        int x = 10; // local variable
+        System.out.println(x);
+        // System.out.println(a); // compilation error
+
+    }
+}
